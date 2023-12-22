5777168572e50390f0cfbbbb064783387c77ff91
DucLUT <minhduc16032004@gmail.com>
2023-12-22 17:17:24 +0200

    small updates

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,6 +17,13 @@ const Content = (props) => {
     </>
   )
 }
+const Total = (props) => {
+  return (
+    <>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
+}
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -32,7 +39,7 @@ const App = () => {
     <Content part = {part1} exercises = {exercises1}/>
     <Content part = {part2} exercises = {exercises2}/>
     <Content part = {part3} exercises = {exercises3}/>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
 
     </>
   )
