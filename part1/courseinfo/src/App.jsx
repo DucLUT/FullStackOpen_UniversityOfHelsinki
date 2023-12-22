565e91c4993b98b9ca7a74776c5d5cfdeb3107da
DucLUT <minhduc16032004@gmail.com>
2023-12-22 21:52:35 +0200

    hihi

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,10 +10,17 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
     <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+    <Part part = {props.part} exercises = {props.exercises}/>
     </>
   )
 }
