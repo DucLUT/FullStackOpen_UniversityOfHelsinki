ee362bce6528d85698a68c813bde2a163d616b64
Duc Duong <Duc.Duong@student.lut.fi>
2023-12-30 17:22:34 +0200

    done 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+const Display = ({text, value}) => {
+  return (
+    <div>{text} {value}</div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <>
+    <button onClick={handleClick}>
+      {text}
+    </button><br />
+    </>
+    )
+  }
+const App = () => {
+
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
+
+  return (
+    <>
+    <h1>Give feedback</h1>
+    <Button handleClick={handleGoodClick} text='good' />
+    <Button handleClick={handleNeutralClick} text='neutral' />
+    <Button handleClick={handleBadClick} text='bad' />
+    <h1>Statistics</h1>
+    <Display text='good' value={good} />
+    <Display text='neutral' value={neutral} />
+    <Display text='bad' value={bad} />
+    <Display text='all' value={total} />
+    </>
+  )
+}
+
+export default App
