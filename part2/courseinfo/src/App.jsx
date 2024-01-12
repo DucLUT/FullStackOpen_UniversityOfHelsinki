e1725a2fc91e60f69621d9c1eaa1de27c09e35f1
Duc Duong <Duc.Duong@student.lut.fi>
2024-01-12 21:53:54 +0200

    done 2.3*

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -29,14 +29,19 @@ const Content = (props) => {
   )
 }
 const Total = (props) => {
-  let a = 0;
-  props.course.parts.forEach(part => {
-    a += part.exercises;
-  });
+  // let a = 0;
+  // props.course.parts.forEach(part => {
+  //   a += part.exercises;
+  // });
+  const parts = props.course.parts
+  const total = parts.reduce((s, p) => {
+    console.log('what is happening', s, p)
+    return s + p.exercises
+  }, 0)
   
   return (
     <>
-      <h3>Total of {a} exercises</h3>
+      <h3>Total of {total} exercises</h3>
     </>
   );
 }
@@ -52,29 +57,54 @@ const Course = (props) => {
   
 }
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
   return (
     <>
-    <Course course = {course}/>
+    <Course course = {courses[0]}/>
     
     </>
   )
