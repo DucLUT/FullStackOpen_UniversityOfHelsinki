459c32c60842f492b51f3edf81b79b672370359d
Duc Duong <minhduc16032004@gmail.com>
2024-01-15 00:44:27 +0200

    2.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -199,17 +199,33 @@ import Render from './component/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length +1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
   return(
     <>
-    <Render notes = {notes}/>
+    <h1>Notes</h1>
+    <div>
+      <button onClick = {() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+    </div>
+    <Render notes = {notesToShow}/>
     <form onSubmit = {addNote}>
       <input value = {newNote} onChange = {handleNoteChange}/>
       <br />
