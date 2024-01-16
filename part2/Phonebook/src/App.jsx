2ea86b54072c8d0f3edc76f89d660b4b31c527d2
Duc Duong <minhduc16032004@gmail.com>
2024-01-16 17:52:51 +0200

    done 2.8

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -6,18 +6,27 @@ import Display from './component/display.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,id: 1 }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)  
+  }
+  const handleChangeNumver = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   const addPeople = (e) => {
     e.preventDefault()
     console.log('button clicked', e.target)
     const peopleObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
     if (persons.some(person => person.name === newName)){
@@ -25,6 +34,7 @@ const App = () => {
     }else{
       setPersons(persons.concat(peopleObject))
       setNewName('')
+      setNewNumber('')
   }
   }
 
@@ -33,6 +43,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit = {addPeople}>
         name: <input value = {newName} onChange = {handleNameChange} />
+        <br/>
+        number: <input value={newNumber} onChange = {handleChangeNumver}/> 
         <br/>
         <button type="submit">add</button>
     
--- part2/Phonebook/src/component/display.jsx
+++ part2/Phonebook/src/component/display.jsx
@@ -1,5 +1,5 @@
 const Display = (props) => {
-    const result = props.persons.map((person) => <p key = {person.id}>{person.name}</p> )
+    const result = props.persons.map((person) => <p key = {person.id}>{person.name} {person.number}</p> )
     return (
         <>
 
