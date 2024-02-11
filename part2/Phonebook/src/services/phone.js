de7b43503a8613ddf36e19dbd2fb5b306fa431d7
Duc Duong <minhduc16032004@gmail.com>
2024-02-12 01:17:48 +0200

    done 2.12

--- part2/Phonebook/src/services/phone.js
+++ part2/Phonebook/src/services/phone.js
@@ -10,5 +10,9 @@ const create = async (newObject) => {
     return axios.post(baseUrl, newObject).then(response => response.data)
 }
 
+const deletePerson = async (id) => {
+    return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
+}
 
-export default {getAll, create}
+
+export default {getAll, create, deletePerson}
