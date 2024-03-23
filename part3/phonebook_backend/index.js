c5e2de98e41e1e6683763fff5a0a35c5ea5bba9c
DucLUT <minhduc16032004@gmail.com>
2024-03-23 23:50:23 +0200

    done 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info',(req,res) => {
+    
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+              <p>${new Date()}</p>`)
+              
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {    
