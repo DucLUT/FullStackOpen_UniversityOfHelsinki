d77a3a67ec994e475f5dfc3895caacf2e0a82b5c
DucLUT <minhduc16032004@gmail.com>
2024-03-25 01:52:46 +0200

    done 3.7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
-persons = [
+app.use(morgan('tiny'))
+let persons = [
     {
         id: 1,
         name: "Arto Hellas",
