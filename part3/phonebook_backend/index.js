4de27f7243382dfbf302e42ab167302b4789873b
DucLUT <minhduc16032004@gmail.com>
2024-03-24 17:20:44 +0200

    done 3.4

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -50,6 +50,12 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    res.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {    
