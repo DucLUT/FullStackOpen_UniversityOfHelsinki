1f88766a578860a978c92d8130c75258757eace1
DucLUT <minhduc16032004@gmail.com>
2024-12-21 22:12:13 +0200

    done 4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -22,7 +23,9 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+  
 
 module.exports = app
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -7,6 +7,10 @@ const blogSchema = new mongoose.Schema({
     likes: {
       type:Number,
       default: 0
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
 })
 
