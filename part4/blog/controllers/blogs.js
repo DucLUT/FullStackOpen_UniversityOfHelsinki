39ca2b00612a05737b6cb338191201569f5bd8e4
DucLUT <minhduc16032004@gmail.com>
2024-12-22 02:53:04 +0200

    done 4.19

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,32 +1,54 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 blogsRouter.get('/', async (req,res,next) => {
     const blogs = await Blog.find({}).populate('user',{userName:1, name:1})
     res.json(blogs)
 })
 
-blogsRouter.post('/', async (req,res,next) => {
+blogsRouter.post('/', async (req, res, next) => {
     const body = req.body
-    if (!body.title || !body.url){
-        return res.status(400).json({error: 'url or title missing'})
+    const token = getTokenFrom(req)
+    let decodedToken
+    try {
+        decodedToken = jwt.verify(token, process.env.SECRET)
+    } catch (e) {
+        return res.status(401).json({ error: 'token invalid' })
     }
-    const user = await User.findById(body.userId)
-    console.log(user)
+
+    if (!decodedToken.id) {
+        return res.status(401).json({ error: 'token invalid' })
+    }
+
+    if (!body.title || !body.url) {
+        return res.status(400).json({ error: 'url or title missing' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes,
+        likes: body.likes || 0,
         user: user._id
     })
+
     try {
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         res.status(201).json(savedBlog)
-    } catch (e){
-        e => next(e)
+    } catch (e) {
+        next(e)
     }
 })
 blogsRouter.put('/:id', async (req, res,next) => {
