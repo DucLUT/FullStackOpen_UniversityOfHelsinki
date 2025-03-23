ae0ea28aa3327194a168aa4e0516cfd7dc2a9f74
DucLUT <minhduc16032004@gmail.com>
2025-03-23 20:48:59 +0200

    done 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,13 +1,23 @@
+import  { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import { setAll } from './reducers/anecdoteReducer'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(setAll(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification /> 
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await axios.get(baseURL)
+    return response.data
+}
+
+export default {getAll}
